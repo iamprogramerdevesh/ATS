12a8e8900e9e5ca461429d05ca3a740e4d4455d2
iamcoderdevesh <154750450+iamprogramerdevesh@users.noreply.github.com>
2024-10-13 08:36:52 +0530

    Client & Server Setup

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import corsOptions from "./src/config/corsOptions.js";
+import credentials from "./src/config/corsCredentials.js";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+//#region CONFIGURATIONS
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 6000;
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(credentials);
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
+app.use(cookieParser());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+//#endregion
+
+//#region MONGOOSE SETUP
+mongoose.connect(process.env.MONGO_LOCAL_URL, {
+  dbName: process.env.DATABASE_NAME
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
+//#endregion
